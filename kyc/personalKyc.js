d413770baa5948cb353ec80e2b82dd90cf827cfa
Iranks20 <rankunda48@gmail.com>
2023-10-07 13:26:54 +0300

    added a relatioonshoip of user id'

--- kyc/personalKyc.js
+++ kyc/personalKyc.js
@@ -35,12 +35,18 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const id = 1;
+    // Generate a random integer ID (e.g., between 1000 and 9999)
+    const generatedId = Math.floor(Math.random() * 9000) + 1000;
 
-    const kycSql = 'INSERT INTO personal_information_kyc (full_name, date_of_birth, nationality, id_number, phone_number, email_address, tax_id_number, source_of_funds) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    // Set the status to "pending"
+    const status = 'pending';
+
+    const kycSql =
+      'INSERT INTO personal_information_kyc (user_id, full_name, date_of_birth, nationality, id_number, phone_number, email_address, tax_id_number, source_of_funds, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     db.query(
       kycSql,
       [
+        generatedId, // Include the generated ID in the user_id field
         full_name,
         date_of_birth,
         nationality,
@@ -49,6 +55,7 @@ router.post(
         email_address,
         tax_id_number,
         source_of_funds,
+        status, // Include the status
       ],
       (err, kycResult) => {
         if (err) {
@@ -58,39 +65,129 @@ router.post(
         }
 
         console.log('KYC data registered successfully');
-        res.status(200).json({ message: 'KYC data registered successfully' });
+
+        // Include the generated ID and status in the response
+        res.status(200).json({
+          message: 'KYC data registered successfully',
+          user_id: generatedId, // Include the generated ID in the response
+          status: status,
+        });
       }
     );
   }
 );
 
+
+
 router.post('/create-company-details', (req, res) => {
   console.log('Incoming request body:', req.body);
-  const { business_license, company_registration_certificate } = req.body;
+  const { user_id, business_license, company_registration_certificate } = req.body;
 
-  if (!business_license || !company_registration_certificate) {
-    return res.status(400).json({ error: 'Business license and company registration certificate are required' });
+  if (!user_id || !business_license || !company_registration_certificate) {
+    return res.status(400).json({ error: 'User ID, business license, and company registration certificate are required' });
   }
+
+  // Set the status to "pending"
+  const status = 'pending';
 
   const insertSql = `
     INSERT INTO company_information_kyc
-    (business_license, company_registration_certificate)
-    VALUES (?, ?)
+    (user_id, business_license, company_registration_certificate, status)
+    VALUES (?, ?, ?, ?)
   `;
 
   db.query(
     insertSql,
-    [business_license, company_registration_certificate],
+    [user_id, business_license, company_registration_certificate, status], // Include user_id and status
     (err, result) => {
       if (err) {
         console.error('Database insertion error:', err);
         res.status(500).json({ error: 'Internal server error' });
       } else {
         console.log('Company details submitted successfully');
-        res.status(200).json({ message: 'Company details submitted successfully' });
+
+        // Include user_id and status in the response
+        res.status(200).json({
+          message: 'Company details submitted successfully',
+          user_id: user_id,
+          status: status,
+        });
       }
     }
   );
 });
+
+router.post('/activate-user', (req, res) => {
+  const { user_id } = req.body;
+
+  // Check if the user_id exists in the personal_information_kyc table
+  const personalKycSql = 'SELECT * FROM personal_information_kyc WHERE id = ?';
+  db.query(personalKycSql, [user_id], (err, personalKycResult) => {
+    if (err) {
+      console.error('Database error:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    // Check if the user_id exists in the company_information_kyc table
+    const companyKycSql = 'SELECT * FROM company_information_kyc WHERE user_id = ?';
+    db.query(companyKycSql, [user_id], (err, companyKycResult) => {
+      if (err) {
+        console.error('Database error:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      // Check if the user_id exists in the user_documents_kyc table
+      const userDocumentsKycSql = 'SELECT * FROM user_documents_kyc WHERE user_id = ?';
+      db.query(userDocumentsKycSql, [user_id], (err, userDocumentsKycResult) => {
+        if (err) {
+          console.error('Database error:', err);
+          return res.status(500).json({ error: 'Internal server error' });
+        }
+
+        // If user_id exists in either table, update status to "active"
+        if (personalKycResult.length > 0 || companyKycResult.length > 0 || userDocumentsKycResult.length > 0) {
+          // Update status to "active" in the personal_information_kyc table
+          if (personalKycResult.length > 0) {
+            const updatePersonalKycSql = 'UPDATE personal_information_kyc SET status = ? WHERE id = ?';
+            db.query(updatePersonalKycSql, ['active', user_id], (err) => {
+              if (err) {
+                console.error('Database error:', err);
+                return res.status(500).json({ error: 'Internal server error' });
+              }
+            });
+          }
+
+          // Update status to "active" in the company_information_kyc table
+          if (companyKycResult.length > 0) {
+            const updateCompanyKycSql = 'UPDATE company_information_kyc SET status = ? WHERE user_id = ?';
+            db.query(updateCompanyKycSql, ['active', user_id], (err) => {
+              if (err) {
+                console.error('Database error:', err);
+                return res.status(500).json({ error: 'Internal server error' });
+              }
+            });
+          }
+
+          // Update status to "active" in the user_documents_kyc table
+          if (userDocumentsKycResult.length > 0) {
+            const updateUserDocumentsKycSql = 'UPDATE user_documents_kyc SET status = ? WHERE user_id = ?';
+            db.query(updateUserDocumentsKycSql, ['active', user_id], (err) => {
+              if (err) {
+                console.error('Database error:', err);
+                return res.status(500).json({ error: 'Internal server error' });
+              }
+            });
+          }
+
+          return res.status(200).json({ message: 'User status updated to "active"' });
+        } else {
+          return res.status(404).json({ error: 'User doesn\'t exist' });
+        }
+      });
+    });
+  });
+});
+
+
 
 module.exports = router;
--- kyc/uploadUser.js
+++ kyc/uploadUser.js
@@ -28,41 +28,47 @@ router.post('/upload-files', upload.fields([
     { name: 'trade_licences', maxCount: 1 }
 ]), (req, res) => {
     try {
+        const { user_id } = req.body; // Add user_id to the request body
+        const status = 'pending'; // Set the status to "pending"
+
         const nationalIDFile = req.files['national_id_card_or_passport'][0];
         const selfieFile = req.files['selfie'][0];
         const proofOfAddressFile = req.files['proof_of_address'][0];
         const tradeLicencesFile = req.files['trade_licences'][0];
 
-    if (!nationalIDFile || !selfieFile || !proofOfAddressFile || !tradeLicencesFile) {
-        return res.status(400).send({ message: 'Please upload all required files.' });
-    }
-
-    const filePaths = {
-        national_id_card_or_passport: nationalIDFile.path,
-        selfie: selfieFile.path,
-        proof_of_address: proofOfAddressFile.path,
-        trade_licences: tradeLicencesFile.path
-    };
-
-    const insertSql = `
-        INSERT INTO user_documents_kyc
-        (national_id_card_or_passport, selfie, proof_of_address, trade_licences)
-        VALUES (?, ?, ?, ?)
-    `;
-
-    db.query(insertSql, [filePaths.national_id_card_or_passport, filePaths.selfie, filePaths.proof_of_address, filePaths.trade_licences], function (err, result) {
-        if (err) {
-            console.error(err);
-            return res.status(500).send({ message: 'Error inserting file paths into the database.' });
+        if (!nationalIDFile || !selfieFile || !proofOfAddressFile || !tradeLicencesFile) {
+            return res.status(400).send({ message: 'Please upload all required files.' });
         }
 
-        return res.send({ message: 'File paths are successfully uploaded and stored.', file_paths: filePaths });
-    });
-} catch (error) {
-    console.error(error);
-    return res.status(500).send({ message: 'An error occurred during file upload and database insertion.' });
-}
+        const filePaths = {
+            national_id_card_or_passport: nationalIDFile.path,
+            selfie: selfieFile.path,
+            proof_of_address: proofOfAddressFile.path,
+            trade_licences: tradeLicencesFile.path
+        };
+
+        const insertSql = `
+            INSERT INTO user_documents_kyc
+            (user_id, national_id_card_or_passport, selfie, proof_of_address, trade_licences, status)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `;
+
+        db.query(insertSql, [user_id, filePaths.national_id_card_or_passport, filePaths.selfie, filePaths.proof_of_address, filePaths.trade_licences, status], function (err, result) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send({ message: 'Error inserting file paths into the database.' });
+            }
+
+            return res.send({ message: 'File paths are successfully uploaded and stored.', file_paths: filePaths, user_id, status });
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ message: 'An error occurred during file upload and database insertion.' });
+    }
 });
+
+module.exports = router;
+
 
 module.exports = router;
 
